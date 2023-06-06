9f7cbc00f4f7e82d165076387d0a63b9ef007429
RuthAdora <adoraruth01@gmail.com>
2023-06-06 11:33:39 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-for x in range(122, 96, -1):
-        if x % 2 != 0:
-                    print("{}".format(chr(x - 32)), end="")
-                        else:
-                                    print("{}".format(chr(x)), end="")
+for char in range(122, 96, -1):
+    if char % 2 != 0:
+        char -= 32
+        print("{}".format(chr(char)), end="")
