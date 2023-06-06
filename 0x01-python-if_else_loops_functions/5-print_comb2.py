3c5b5362d6c3d9ed4a8be4c49a52e3df06398e8f
RuthAdora <adoraruth01@gmail.com>
2023-06-06 10:32:40 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for x in range(0, 99):
-        print("{:0>2d}".format(x), end=", ")
-
-        print(x+1)
+for num in range(0, 100):
+    print("{:d}".format(num), end='\n' if num == 99 else ", ")
