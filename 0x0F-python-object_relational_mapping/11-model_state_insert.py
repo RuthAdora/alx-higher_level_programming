8fdd9ee5852a6e60afa2d18a375d892585e41205
RuthAdora <adoraruth01@gmail.com>
2023-09-18 12:18:24 +0300

    object relational mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
- adds the State object "Louisiana" to the database hbtn_0e_6_usa
+Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+Usage: ./11-model_state_insert.py <mysql username> /
+                                  <mysql password> /
+                                  <database name>
 """
-
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                               .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                                pool_pre_ping=True)
+     Session = sessionmaker(bind=engine)
+     session = Session()
 
-    obj = State(name="Louisiana")
-    session.add(obj)
-    session.commit()
-    print(obj.id)
+     louisiana = State(name="Louisiana")
+     session.add(louisiana)
+     session.commit()
+     print(louisiana.id)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 """
-Defines a state model that contain the class definition
- of a State and an instance Base = declarative_base()
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
 """
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
-    """
-    inherits from Base Tips
-    links to the MySQL table states
-    class attribute id that represents a column
-     of an auto-generated, unique integer, can't
-      be null and is a primary key
-    class attribute name that represents a column
-     of a string with maximum 128 characters and
-      can't be null
-
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+      """Represents a state for a MySQL database.
+      __tablename__ (str): The name of the MySQL table to store States.
+      id (sqlalchemy.Integer): The state's id.
+      name (sqlalchemy.String): The state's name.
+      """
+      __tablename__ = "states"
+      id = Column(Integer, primary_key=True)
+      name = Column(String(128), nullable=False)
